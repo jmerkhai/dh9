e610ab5c935071dfe399a14382cb4fcb1fc00094
Qwanton19 <quinton.mccrory@gmail.com>
2023-01-14 22:39:12 -0500

    troubleshooted it only working if they were in same playlist position

--- main.py
+++ main.py
@@ -11,41 +11,41 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 playlistLink1 = input("Enter the first playlist link: ")
 playlist_URI1 = playlistLink1.split("/")[-1].split("?")[0]
 
-playlistData = sp.playlist(playlist_URI1)
-json.dumps(playlistData, indent=4)
+results = sp.playlist(playlist_URI1)
+json.dumps(results, indent=4)
 
 songTitle1 = []
 songArtist1 = []
 
-for item in playlistData['tracks']['items']:
+for item in results['tracks']['items']:
     songTitle1.append(item['track']['name'])
     songArtist1.append(item['track']['artists'][0]['name'])
-
-print(songTitle1)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 playlistLink2 = input("Enter the second playlist link: ")
 playlist_URI2 = playlistLink2.split("/")[-1].split("?")[0]
 
-playlistData = sp.playlist(playlist_URI2)
-json.dumps(playlistData, indent=4)
+results = sp.playlist(playlist_URI2)
+json.dumps(results, indent=4)
 
 songTitle2 = []
 songArtist2 = []
 
-for item in playlistData['tracks']['items']:
+for item in results['tracks']['items']:
     songTitle2.append(item['track']['name'])
     songArtist2.append(item['track']['artists'][0]['name'])
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 print("Songs in common: ")
 
-if len(songTitle1) < len(songTitle2):
+if len(songTitle1) <= len(songTitle2):
     for x in range(len(songTitle1)):
-        if songTitle1[x] == songTitle2[x] and songArtist1[x] == songArtist2[x]:
-            print(songTitle1[x] + " - " + songArtist1[x])
-else:
+        for y in range(len(songTitle2)):
+            if songTitle1[x] == songTitle2[y] and songArtist1[x] == songArtist2[y]:
+                print(songTitle1[x] + " - " + songArtist1[x])
+elif len(songTitle1) > len(songTitle2):
     for x in range(len(songTitle2)):
-        if songTitle1[x] == songTitle2[x] and songArtist1[x] == songArtist2[x]:
-            print(songTitle1[x] + " - " + songArtist1[x])
+        for y in range(len(songTitle1)):
+            if songTitle1[y] == songTitle2[x] and songArtist1[y] == songArtist2[x]:
+                print(songTitle1[y] + " - " + songArtist1[y])
