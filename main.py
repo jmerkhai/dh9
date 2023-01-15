c4488d69412ade31d035e6c0a9647ed27f87caa6
Qwanton19 <quinton.mccrory@gmail.com>
2023-01-15 11:05:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import spotipy
 import json
 from spotipy.oauth2 import SpotifyClientCredentials
 cid = '6b0fcacdc9e24abaa5f8da3144748059'
-secret = '82e1ec90361c474ab40349ea922b5958'
+secret = ''
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
@@ -12,6 +12,7 @@ playlistLink1 = input("Enter the first playlist link: ")
 playlist_URI1 = playlistLink1.split("/")[-1].split("?")[0]
 
 results = sp.playlist(playlist_URI1)
+results = results + sp.playlist(playlist_URI1)
 json.dumps(results, indent=4)
 
 songTitle1 = []
@@ -20,6 +21,7 @@ songArtist1 = []
 for item in results['tracks']['items']:
     songTitle1.append(item['track']['name'])
     songArtist1.append(item['track']['artists'][0]['name'])
+print(len(songTitle1))
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
@@ -37,10 +39,13 @@ for item in results['tracks']['items']:
     songArtist2.append(item['track']['artists'][0]['name'])
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
 print("Songs in common: ")
 
 if len(songTitle1) <= len(songTitle2):
     for x in range(len(songTitle1)):
+        print(songTitle1[x])
         for y in range(len(songTitle2)):
             if songTitle1[x] == songTitle2[y] and songArtist1[x] == songArtist2[y]:
                 print(songTitle1[x] + " - " + songArtist1[x])
