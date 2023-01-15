4239fecd4182b6ca4cebff642c75f7f3881e8acf
Qwanton19 <quinton.mccrory@gmail.com>
2023-01-14 19:53:40 -0500

    actually working playlist union
    a

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
-import json
 import spotipy
-import webbrowser
+import json
+from spotipy.oauth2 import SpotifyClientCredentials
+cid = '6b0fcacdc9e24abaa5f8da3144748059'
+secret = '82e1ec90361c474ab40349ea922b5958'
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-username = 'fo9t5nsb0devz564jqr7di6yr'
-clientID = '6b0fcacdc9e24abaa5f8da3144748059'
-clientSecret = '82e1ec90361c474ab40349ea922b5958'
-redirect_uri = 'http://google.com/callback/'
+playlistLink1 = input("Enter the first playlist link: ")
+playlist_URI1 = playlistLink1.split("/")[-1].split("?")[0]
 
-oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri)
-token_dict = oauth_object.get_access_token()
-token = token_dict['access_token']
-spotifyObject = spotipy.Spotify(auth=token)
-user_name = spotifyObject.current_user()
+playlistData = sp.playlist(playlist_URI1)
+json.dumps(playlistData, indent=4)
 
-# To print the response in readable format.
-print(json.dumps(user_name, sort_keys=True, indent=4))
+songTitle1 = []
+songArtist1 = []
 
-while True:
-    print("Welcome to the project, " + user_name['display_name'])
-    print("0 - Exit the console")
-    print("1 - Search for a Song")
-    user_input = int(input("Enter Your Choice: "))
-    if user_input == 1:
-        search_song = input("Enter the song name: ")
-        results = spotifyObject.search(search_song, 1, 0, "track")
-        songs_dict = results['tracks']
-        song_items = songs_dict['items']
-        song = song_items[0]['external_urls']['spotify']
-        webbrowser.open(song)
-        print('Song has opened in your browser.')
-    elif user_input == 0:
-        print("Good Bye, Have a great day!")
-        break
-    else:
-        print("Please enter valid user-input.")
-        
+for item in playlistData['tracks']['items']:
+    songTitle1.append(item['track']['name'])
+    songArtist1.append(item['track']['artists'][0]['name'])
+
+print(songTitle1)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+playlistLink2 = input("Enter the second playlist link: ")
+playlist_URI2 = playlistLink2.split("/")[-1].split("?")[0]
+
+playlistData = sp.playlist(playlist_URI2)
+json.dumps(playlistData, indent=4)
+
+songTitle2 = []
+songArtist2 = []
+
+for item in playlistData['tracks']['items']:
+    songTitle2.append(item['track']['name'])
+    songArtist2.append(item['track']['artists'][0]['name'])
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+print("Songs in common: ")
+
+if len(songTitle1) < len(songTitle2):
+    for x in range(len(songTitle1)):
+        if songTitle1[x] == songTitle2[x] and songArtist1[x] == songArtist2[x]:
+            print(songTitle1[x] + " - " + songArtist1[x])
+else:
+    for x in range(len(songTitle2)):
+        if songTitle1[x] == songTitle2[x] and songArtist1[x] == songArtist2[x]:
+            print(songTitle1[x] + " - " + songArtist1[x])
