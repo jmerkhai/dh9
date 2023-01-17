84a8fda8f50de31b28ad992f518913f413f14252
Qwanton19 <quinton.mccrory@gmail.com>
2023-01-17 17:36:22 -0500

    fixed code

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ playlistLink1 = input("Enter the first playlist link: ")
 playlist_URI1 = playlistLink1.split("/")[-1].split("?")[0]
 
 results = sp.playlist(playlist_URI1)
-results = results + sp.playlist(playlist_URI1)
 json.dumps(results, indent=4)
 
 songTitle1 = []
@@ -21,7 +20,6 @@ songArtist1 = []
 for item in results['tracks']['items']:
     songTitle1.append(item['track']['name'])
     songArtist1.append(item['track']['artists'][0]['name'])
-print(len(songTitle1))
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
@@ -45,7 +43,6 @@ print("Songs in common: ")
 
 if len(songTitle1) <= len(songTitle2):
     for x in range(len(songTitle1)):
-        print(songTitle1[x])
         for y in range(len(songTitle2)):
             if songTitle1[x] == songTitle2[y] and songArtist1[x] == songArtist2[y]:
                 print(songTitle1[x] + " - " + songArtist1[x])
